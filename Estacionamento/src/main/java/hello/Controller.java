18db1fccf0065b5ee05ae81325f0c8ae7ac63e48
Giuliano Bertoti <giuliano.bertoti@gmail.com>
2017-06-05 10:01:19 -0300

    update

---
+++ Estacionamento/src/main/java/hello/Controller.java
@@ -0,0 +1,31 @@
+package hello;
+
+import static spark.Spark.get;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class Controller {
+	
+	private Model model;
+	
+	
+	public Controller(Model model){
+		this.model = model;
+	}
+	
+	public void buscarCarro(){
+		get("/carro/:modelo/:marca/:cor", (req, res) -> {
+		
+			Especificacao espec = new Especificacao(req.params(":modelo"), req.params(":marca"), req.params(":cor"));	
+			List<Carro> carrosEncontrados = model.buscarEspecificacao(espec);	
+			return new Gson().toJson(carrosEncontrados);
+			
+		});
+	}
+	
+	
+	
+
+}
---
+++ Estacionamento/src/main/java/hello/MainServer.java
@@ -0,0 +1,36 @@
+package hello;
+
+import static spark.Spark.*;
+
+
+public class MainServer {
+	
+	final static Model model = new Model();
+	
+    public static void main(String[] args) {
+
+		// Get port config of heroku on environment variable
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        port(port);
+
+		//Servir conteudo html, css e javascript
+		staticFileLocation("/static");
+
+		inicializarCarros();
+
+		Controller controller = new Controller(model);
+		
+		controller.buscarCarro();
+		
+    }
+    
+    public static void inicializarCarros(){
+    	model.addCarro(new Carro("AAA-1111", new Especificacao("gol", "vw", "verde")));
+    }
+}
